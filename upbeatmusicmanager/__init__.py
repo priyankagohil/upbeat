db1f08ebfcc0db4c5012451b33f614788939fe56
Priyanka Gohil <priyankapgohil@gmail.com>
2020-06-18 12:08:14 +0530

    core views and templates and error handler

--- upbeatmusicmanager/__init__.py
+++ upbeatmusicmanager/__init__.py
@@ -3,3 +3,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from upbeatmusicmanager.core.views import core
+from upbeatmusicmanager.error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
---
+++ upbeatmusicmanager/core/views.py
@@ -0,0 +1,10 @@
+# core/views.py
+
+from flask import render_template,request,Blueprint
+
+core = Blueprint('core',__name__)
+
+@core.route('/')
+def index():
+    # MORE TO COME!
+    return render_template('index.html')
